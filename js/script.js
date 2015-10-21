4b693e546fc1d791d2fda0d747c7b1a7857338d6
Eric Kim <erickim91@gmail.com>
2015-10-21 13:42:42 -0400

    1:42 pm

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
 
     //sprite
     //params are pixel of width and height
-    game.load.spritesheet("dude", "assets/dude1.png", 32, 48);
+    game.load.spritesheet("zombie", "assets/zombie.png", 32, 64);
 
     //audio
     game.load.audio("sound", "assets/8bit-thriller.mp3");
@@ -26,14 +26,42 @@
     // var music = game.add.audio("sound")
     // music.play();
 
-    game.add.image(0,0, "dirt");
-    game.add.image(0,0, "bone");
-    game.add.sprite(0,500, "dude");
+    game.add.image(0, 0, "dirt");
+
+    //set graphics
 
     platforms = game.add.group();
     //enable physics on platform (gives collision to this item)
     platforms.enableBody = true;
-    
+
+    //creates ground, params are x-position, y-position, file
+    var ground = platforms.create(0, game.world.height - 40, "bone");
+
+    //scales item (x, y)
+    ground.scale.setTo(2, 2);
+
+    //immovable holds item in place, providing collision for ground after jumping
+    ground.body.immovable = true;
+
+    var ledge = platforms.create(500, 500, "bone");
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(300, 300, "bone");
+    ledge.body.immovable = true;
+
+    //set player
+
+    var player = game.add.sprite(32, game.world.height - 90, "zombie");
+
+    //enables physics for player
+    game.physics.arcade.enable(player);
+
+    //gives player physics properties
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+
+
 
 
 
