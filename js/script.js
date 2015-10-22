77638a95440380955f0f3029f45cc137d328850c
Nathai <spontandom@gmail.com>
2015-10-21 22:31:51 -0400

    removed duplicated gameEnd function

--- js/script.js
+++ js/script.js
@@ -234,28 +234,4 @@
   } // startGame
 
 
-  function gameEnd(){
-    var game = new Phaser.Game(1280,720, Phaser.AUTO, 'game', {preload: preload, create: create});
-
-    function preload() {
-      game.load.image('mainpage', "assets/main.png");
-      game.load.image('again', "/assets/again.png");
-      game.load.image('over', "assets/gameover.png");
-    };
-
-    function create() {
-      background = game.add.image(0, 0, "mainpage").scale.setTo(1.25,1);
-      //var start = game.add.text(16, 16, 'Start Game', {fill: '#FFF'});
-      game.add.image(80, 175, 'over').scale.setTo(0.5,0.5);
-      var button = game.add.button(game.world.centerX - 105, 325, 'again', actionOnClick, this, 2, 1, 0);
-
-    };
-
-    function actionOnClick() {
-      document.getElementById('game').innerHTML = ''
-      window.onload= startGame();
-    }
-  }
-
-
 })();
